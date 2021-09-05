dc2ac1f0bf6f48d7a74f7b7f4425e2e93cc64a2a
andreeeichuk <andreeeichuk@gmail.com>
2021-09-05 18:59:04 +0300

    Add card position and set

---
+++ Assets/Scripts/CommonCCG/Models/CardPosition.cs
@@ -0,0 +1,14 @@
+﻿namespace UniversalCCG.CommonCCG.Models
+{
+	public struct CardPosition
+	{
+		public CardSet Set { get; }
+		public int Index { get; }
+
+		public CardPosition(CardSet cardSet, int index)
+		{
+			Set = cardSet;
+			Index = index;
+		}
+	}
+}
---
+++ Assets/Scripts/CommonCCG/Models/CardSet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace UniversalCCG.CommonCCG.Models
+{
+	public abstract class CardSet
+	{
+		private List<Card> _cards;
+
+		protected CardSet()
+		{
+			_cards = new List<Card>();
+		}
+	}
+}
