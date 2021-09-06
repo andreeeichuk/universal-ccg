c554703e65d6164d017c5fb7b08727714c2722cf
andreeeichuk <andreeeichuk@gmail.com>
2021-09-06 21:03:11 +0300

    Add card 2d position

---
+++ Assets/Scripts/CommonCCG/Models/Cards/Card2DPosition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UniversalCCG.CommonCCG.Models.CardSets;
+
+namespace UniversalCCG.CommonCCG.Models.Cards
+{
+	public class Card2DPosition<TCard> where TCard : Card
+	{
+		public I2DCardSet<TCard> Set { get; }
+
+		public Vector2Int Coordinates { get; }
+
+		public Card2DPosition(I2DCardSet<TCard> set, Vector2Int coordinates)
+		{
+			Set = set;
+			Coordinates = coordinates;
+		}
+	}
+}
